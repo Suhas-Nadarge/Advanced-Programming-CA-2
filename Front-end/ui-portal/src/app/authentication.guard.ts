b7db6ac94ceea9636bcec09b8fa341c3ba3102de
Suhas Nadarge <suhas.nadarge@mnx.com>
2021-12-19 21:36:36 +0000

    Added comments and removed commented code

--- Front-end/ui-portal/src/app/authentication.guard.ts
+++ Front-end/ui-portal/src/app/authentication.guard.ts
@@ -1,19 +1,15 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationGuard implements CanActivate {
-  // canActivate(
-  //   route: ActivatedRouteSnapshot,
-  //   state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-  //   return true;
-  // }
+  
   constructor(public route:Router){}
 
-
+// Refered and implmented by Suhas from https://www.angular.io
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot) {
